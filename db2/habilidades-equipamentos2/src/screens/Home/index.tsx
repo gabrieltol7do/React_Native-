b04016bc99ae301c7655b7c2ac62aecb4c6d24c0
Gabriel Toledo <gabrieltoledo4629@gmail.com>
2024-11-21 15:18:07 -0300

    separate css

--- db2/habilidades-equipamentos2/src/screens/Home/index.tsx
+++ db2/habilidades-equipamentos2/src/screens/Home/index.tsx
@@ -1,12 +1,11 @@
-import { Image, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-
+import { Image, ScrollView, Text, TextInput, View } from "react-native";
 import boleto from "../../assets/boleto.png";
 import dinheiro from "../../assets/dinheiro.png";
 import Logo from "../../assets/logo.png";
 import Mastercard from "../../assets/mastercard.png";
 import pix from "../../assets/pix.png";
 import Setting from "../../assets/setting.png";
-
+import { styles } from "./style"; // Importação dos estilos
 
 export function Home() {
   return (
@@ -63,115 +62,3 @@ export function Home() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#820AD1",
-    alignItems: "center",
-  },
-  content: {
-    width: "100%",
-    paddingHorizontal: 30,
-  },
-  header: {
-    paddingTop: 40,
-    paddingBottom: 10,
-    width: "100%",
-    justifyContent: "space-between",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  card: {
-    width: "100%",
-    height: 200,
-    borderRadius: 20,
-    elevation: 5,
-    backgroundColor: "#9500F6",
-    justifyContent: "space-between",
-    padding: 20,
-  },
-  cardHeader: {
-    justifyContent: "space-between",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  cardFooter: {
-    justifyContent: "space-between",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  cardFooterText: {
-    fontSize: 24,
-    color: "#FFF",
-    fontWeight: "600",
-    lineHeight: 36,
-  },
-  cardDetails: {
-    padding: 20,
-    marginTop: 20,
-    width: "100%",
-    height: 120,
-    borderRadius: 20,
-    elevation: 5,
-    backgroundColor: "#9500F6",
-  },
-  cardDetailsHeader: {
-    justifyContent: "space-between",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  cardDetailsHeaderText: {
-    color: "#FFF",
-    fontSize: 14,
-    lineHeight: 21,
-    fontWeight: "600",
-  },
-  cardDetailsTextValue: {
-    color: "#FFF",
-    fontSize: 36,
-    lineHeight: 54,
-    fontWeight: "600",
-  },
-  input: {
-    height: 40,
-    width: 120,
-    backgroundColor: "#FFF",
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    fontSize: 14,
-    color: "#000",
-  },
-  footer: {
-    width: "100%",
-    paddingTop: 32,
-  },
-  footerText: {
-    marginLeft: 50,
-    color: "#FFF",
-    fontSize: 14,
-    lineHeight: 21,
-    fontWeight: "600",
-  },
-  footerScrollCardContainer: {
-    width: "100%",
-    height: 300,
-    paddingHorizontal: 19,
-  },
-  footerCard: {
-    marginTop: 37,
-    marginLeft: 8,
-    width: 100,
-    height: 127,
-    elevation: 5,
-    borderRadius: 27,
-    backgroundColor: "#9500F6",
-    justifyContent: "space-between",
-    padding: 15,
-  },
-  footerCardText: {
-    color: "#FFF",
-    fontSize: 12,
-    fontWeight: "600",
-  },
-});
---
+++ db2/habilidades-equipamentos2/src/screens/Home/style.ts
@@ -0,0 +1,113 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#820AD1",
+    alignItems: "center",
+  },
+  content: {
+    width: "100%",
+    paddingHorizontal: 30,
+  },
+  header: {
+    paddingTop: 40,
+    paddingBottom: 10,
+    width: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  card: {
+    width: "100%",
+    height: 200,
+    borderRadius: 20,
+    elevation: 5,
+    backgroundColor: "#9500F6",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  cardHeader: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cardFooter: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cardFooterText: {
+    fontSize: 24,
+    color: "#FFF",
+    fontWeight: "600",
+    lineHeight: 36,
+  },
+  cardDetails: {
+    padding: 20,
+    marginTop: 20,
+    width: "100%",
+    height: 120,
+    borderRadius: 20,
+    elevation: 5,
+    backgroundColor: "#9500F6",
+  },
+  cardDetailsHeader: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cardDetailsHeaderText: {
+    color: "#FFF",
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: "600",
+  },
+  cardDetailsTextValue: {
+    color: "#FFF",
+    fontSize: 36,
+    lineHeight: 54,
+    fontWeight: "600",
+  },
+  input: {
+    height: 40,
+    width: 120,
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    fontSize: 14,
+    color: "#000",
+  },
+  footer: {
+    width: "100%",
+    paddingTop: 32,
+  },
+  footerText: {
+    marginLeft: 50,
+    color: "#FFF",
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: "600",
+  },
+  footerScrollCardContainer: {
+    width: "100%",
+    height: 300,
+    paddingHorizontal: 19,
+  },
+  footerCard: {
+    marginTop: 37,
+    marginLeft: 8,
+    width: 100,
+    height: 127,
+    elevation: 5,
+    borderRadius: 27,
+    backgroundColor: "#9500F6",
+    justifyContent: "space-between",
+    padding: 15,
+  },
+  footerCardText: {
+    color: "#FFF",
+    fontSize: 12,
+    fontWeight: "600",
+  },
+});
